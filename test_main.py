ee90e1852b42d8ff435894bf8d860810cce547a6
last_name <artemdashkov@gmail.com>
2023-12-20 19:02:19 +0300

    update files

--- test_main.py
+++ test_main.py
@@ -12,16 +12,9 @@ def test_predict():
     response = client.post("/predict?text=" + text, json={"text": text})
 
     # Проверка статуса ответа
-    assert response.status_code == 200
+    assert response.status_code == 200, "Not status code 200"
 
     predictions = response.json()
 
     # Проверка типа содержимого ответа
-    assert response.headers["content-type"] == "application/json"
-
-    # Проверка корректности ответа
-    assert isinstance(predictions, list)
-    if predictions:  # Проверка наличия элементов в списке predictions
-        assert isinstance(predictions[0], list)
-    for prob in predictions[0]:
-        assert isinstance(prob, float)
+    assert response.headers["content-type"] == "application/json", "Not json application"
